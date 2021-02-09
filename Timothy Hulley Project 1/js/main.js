d312777a3bca2f14174e4f299ae7ae1a7cc5094e
tjrhulley <57766828+tjrhulley@users.noreply.github.com>
2021-02-09 11:13:38 -0500

    Update main.js

--- Timothy Hulley Project 1/js/main.js
+++ Timothy Hulley Project 1/js/main.js
@@ -14,7 +14,7 @@ import "./phaser.js";
 class gameScene extends Phaser.Scene {
     
     var player;
-	var houses;
+	//var houses;
 	
 	constructor() {
         super();
@@ -62,7 +62,7 @@ class gameScene extends Phaser.Scene {
         // This function returns the rotation angle that makes it visually match its
         // new trajectory.
         //this.bouncy.rotation = this.physics.accelerateToObject( this.bouncy, this.input.activePointer, 500, 500, 500 );
-		
+		/*
 		if (cursors.left.isDown) {
 			player.setVelocityX(-160);
 		} 
@@ -75,6 +75,7 @@ class gameScene extends Phaser.Scene {
 		else if (cursors.down.isDown) {
 			player.setVelocityY(160);
 		}
+		*/
     }
 }
 
