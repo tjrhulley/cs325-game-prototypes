78be13c526cf989fcbcdd4601643f178096f4d7d
tjrhulley <57766828+tjrhulley@users.noreply.github.com>
2021-02-11 13:32:41 -0500

    Update main.js

--- Timothy Hulley Project 1/js/main.js
+++ Timothy Hulley Project 1/js/main.js
@@ -180,7 +180,7 @@ function create ()
             switch (this.heading)
             {
                 case LEFT:
-                    this.headPosition.x = Phaser.Math.Wrap(this.headPosition.x - 1, -20, 40);
+                    this.headPosition.x = Phaser.Math.Wrap(this.headPosition.x - 1, -5, 35); //set the values to 0 and 40 if this doesn't work
                     break;
 
                 case RIGHT:
