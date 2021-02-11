2f18a05dec181ce6da8195ad418849e1741ee4db
tjrhulley <57766828+tjrhulley@users.noreply.github.com>
2021-02-11 02:56:57 -0500

    Update main.js

--- Timothy Hulley Project 1/js/main.js
+++ Timothy Hulley Project 1/js/main.js
@@ -76,8 +76,7 @@ function create ()
             this.body = scene.add.group();
 
             this.head = this.body.create(x * 16, y * 16, 'body');
-            this.head.setOrigin(0);
-			this.head.anchor.setTo(0.5);
+            this.head.setOrigin(0, 0);
 
             this.alive = true;
 
