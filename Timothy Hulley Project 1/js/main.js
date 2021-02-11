53f8b6630bc227673bdaaf7367d17a3731204591
tjrhulley <57766828+tjrhulley@users.noreply.github.com>
2021-02-11 00:22:05 -0500

    Update main.js

--- Timothy Hulley Project 1/js/main.js
+++ Timothy Hulley Project 1/js/main.js
@@ -7,18 +7,27 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload:
 
 function preload() {
 
-    game.load.image('ball','assets/sprites/shinyball.png');
+    game.load.image( 'pizzaLegs', 'assets/Pizza dude-1.png.png' );
+	game.load.image( 'pizzaTorso', 'assets/Pizza dude-2.png.png' );
+	game.load.image( 'skyTEMP', 'assets/sky.png' );
 
 }
 
-var snakeHead; //head of snake sprite
-var snakeSection = new Array(); //array of sprites that make the snake body sections
-var snakePath = new Array(); //arrary of positions(points) that have to be stored for the path the sections follow
-var numSnakeSections = 30; //number of snake body sections
-var snakeSpacer = 6; //parameter that sets the spacing between sections
+var pizzaDude;
+//var snakeSection = new Array(); //array of sprites that make the snake body sections
+//var snakePath = new Array(); //arrary of positions(points) that have to be stored for the path the sections follow
+//var numSnakeSections = 30; //number of snake body sections
+//var snakeSpacer = 6; //parameter that sets the spacing between sections
 
 function create() {
 
+	this.add.image(400, 300, 'skyTEMP');
+	this.add.image(400, 584, 'pizzaLegs');
+	
+	pizzaDude = this.add.sprite(400, 568, 'pizzaTorso');
+
+	/*
+	
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     game.world.setBounds(0, 0, 800, 600);
@@ -42,11 +51,25 @@ function create() {
     {
         snakePath[i] = new Phaser.Point(400, 300);
     }
-
+	*/
 }
 
 function update() {
 
+	if (cursors.left.isDown) {
+		pizzaDude.setVelocityX(-160);
+	} 
+	else if (cursors.right.isDown) {
+		pizzaDude.setVelocityX(160);
+	}
+	else if (cursors.up.isDown) {
+		pizzaDude.setVelocityY(-160);
+	}
+	else if (cursors.down.isDown) {
+		pizzaDude.setVelocityY(160);
+	}
+
+	/*
     snakeHead.body.velocity.setTo(0, 0);
     snakeHead.body.angularVelocity = 0;
 
@@ -78,11 +101,12 @@ function update() {
     {
         snakeHead.body.angularVelocity = 300;
     }
+	*/
 
 }
 
 function render() {
-
+	/*
     game.debug.spriteInfo(snakeHead, 32, 32);
-
+	*/
 }
