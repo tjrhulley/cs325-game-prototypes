0085bf5f1283fa264e412b06df69f19bcfcca560
tjrhulley <57766828+tjrhulley@users.noreply.github.com>
2021-03-30 00:35:04 -0400

    Changes because the server won't fucking work!

---
+++ Timothy Hulley Iterative Project 1/js/main.js
@@ -0,0 +1,64 @@
+import "./phaser.js";
+
+var config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 600,
+    parent: 'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var player;
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('car', 'assets/car1.png'); //load an image for a car
+}
+
+function create ()
+{
+    //this.add.image(400, 300, 'sky');
+
+    // platforms.create(600, 400, 'ground');
+    // platforms.create(50, 250, 'ground');
+    // platforms.create(750, 220, 'ground');
+
+    player = this.add.sprite(100, 450, 'car');
+	
+    player.setCollideWorldBounds(true);
+}
+
+function update ()
+{
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+}
