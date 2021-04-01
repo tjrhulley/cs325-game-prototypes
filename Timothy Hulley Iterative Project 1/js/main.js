d730295fe8a677fb79c932220b8d6e1dc369b34a
tjrhulley <57766828+tjrhulley@users.noreply.github.com>
2021-04-01 01:51:00 -0400

    All the cool shit

--- Timothy Hulley Iterative Project 1/js/main.js
+++ Timothy Hulley Iterative Project 1/js/main.js
@@ -8,7 +8,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 0 },
             debug: false
         }
     },
@@ -19,49 +19,256 @@ var config = {
     }
 };
 
+//Look up demos for randomly spawning objects and despawning objects when they go offscreen.
+
+//Game objects
 var player;
+
+//Hud stuff
+var score = 0;
+var roadGif;
+
+//Game systems
+const minRoad = 200;
+const maxRoad = 600;
+var maxTime = 1300;
 var cursors;
+var gameStep = 0;
+/*
+-0: Game has not started. Press to start
+-1: Robber's turn
+-2: Robber crashed or time is up. Press to start next turn
+-3: Cop's start screen
+-4: Cop's turn
+-5: Start over
+*/
+var objPos = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var objGroup;
+var objTracker = 0;
+var moneyPos = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var moneyGroup;
+var moneyTracker = 0;
+var timer = 0;
+var timedEvent1;
+var timedEvent2;
+
+var DEBUGtext;
 
 var game = new Phaser.Game(config);
 
 function preload ()
 {
     this.load.image('car', 'assets/car1.png'); //load an image for a car
+	this.load.image('police', 'assets/car2.png'); //load an image for a police car
+	this.load.image('cone', 'assets/cone.png');//load image for obsticles
+	this.load.spritesheet('money', 'assets/money.png', { frameWidth: 16, frameHeight: 16}); //load image for money
+	this.load.image('roadStill', 'assets/roadStill.png');//load image for stationary road
+	this.load.spritesheet('roadGif', 'assets/roadGif.png', {frameWidth: 800, frameHeight: 600}); //load image for road gif
 }
 
 function create ()
 {
-    //this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, 'roadStill');
+	
+	this.anims.create({
+        key: 'roadAnim',
+        frames: this.anims.generateFrameNumbers('roadGif', { start: 0, end: 8 }),
+        frameRate: 14,
+        repeat: -1
+    });
+	
+	roadGif = this.add.sprite(400, 300, 'roadGif');
+	roadGif.play({key: 'roadAnim'});
+	roadGif.setVisible(false);
+	
+	this.anims.create({
+        key: 'coinSpin',
+        frames: this.anims.generateFrameNumbers('money', { start: 0, end: 1 }),
+        frameRate: 2,
+        repeat: -1
+    });
+	
+	this.physics.world.setBoundsCollision(true, true, false, false);
 
-    // platforms.create(600, 400, 'ground');
-    // platforms.create(50, 250, 'ground');
-    // platforms.create(750, 220, 'ground');
-
-    player = this.physics.add.sprite(100, 450, 'car');
+    player = this.physics.add.image(400, 450, 'car');
 	
     player.setCollideWorldBounds(true);
+	player.setScale(0.2);
+	player.setVelocityY(0);
 	
 	cursors = this.input.keyboard.createCursorKeys();
+	
+	for (var i = 0; i < objPos.length; i++) {
+		objPos[i] = Phaser.Math.Between(minRoad, maxRoad);
+	}
+	
+	objGroup = this.physics.add.group({
+		defaultKey: 'cone',
+		maxSize: 20
+	});
+	
+	moneyGroup = this.physics.add.group({
+		defaultKey: 'money',
+		maxSize: 10
+	});
+	
+	this.physics.add.overlap(player, objGroup, crash);
+	this.physics.add.overlap(player, moneyGroup, collectCoin);
+	
+	this.input.on('pointerup', function (pointer) {
+		if (gameStep === 0) {
+			timedEvent1 = this.time.addEvent({
+				delay: 1000,
+				loop: true,
+				callback: objEvent
+			});
+			
+			timedEvent2 = this.time.addEvent({
+				delay: 1500,
+				loop: true,
+				callback: dropMoneyEvent
+			});
+			roadGif.setVisible(true);
+			gameStep++;
+		} else if (gameStep === 2) {
+			player.y = -2000;
+			player = this.physics.add.image(400, 450, 'police');
+			player.setCollideWorldBounds(true);
+			player.setScale(0.2);
+			player.setVelocityY(0);
+			
+			this.physics.add.overlap(player, objGroup, crash);
+			this.physics.add.overlap(player, moneyGroup, collectCoin);
+			
+			objGroup.clear(true);
+			moneyGroup.clear(true);
+			gameStep++;
+		} else if (gameStep === 3) {
+			timedEvent1 = this.time.addEvent({
+				delay: 1000,
+				loop: true,
+				callback: objEvent
+			});
+	
+			timedEvent2 = this.time.addEvent({
+				delay: 1500,
+				loop: true,
+				callback: spawnMoneyEvent
+			});
+			roadGif.setVisible(true);
+			gameStep++;
+		}
+		
+	}, this);
+	
+	DEBUGtext = this.add.text(16,16, '');
 }
 
 function update ()
 {
-    if (cursors.left.isDown)
-    {
-        player.setVelocityX(-160);
+	if (gameStep === 1 || gameStep === 4) {
+		Phaser.Actions.IncY(objGroup.getChildren(), 10);
+		Phaser.Actions.IncY(moneyGroup.getChildren(), 10);
+	}
 
-        player.anims.play('left', true);
-    }
-    else if (cursors.right.isDown)
-    {
-        player.setVelocityX(160);
+    objGroup.children.iterate(function (cone) {
+		if (cone.y > 600) {
+            objGroup.killAndHide(cone);
+        }
+    });
+	
+	moneyGroup.children.iterate(function (money) {
+        if (money.y > 600) {
+            moneyGroup.killAndHide(money);
+        }
+    });
+	
+	if (gameStep === 1 || gameStep === 4) {
+		if (cursors.left.isDown)
+		{
+			player.setVelocityX(-200);
+		}
+		else if (cursors.right.isDown)
+		{
+			player.setVelocityX(200);
+		}
+		else
+		{
+			player.setVelocityX(0);
+		}
+		timer++;
+		if (timer >= maxTime) {
+			crash();
+		}
+	}
+}
 
-        player.anims.play('right', true);
-    }
-    else
-    {
-        player.setVelocityX(0);
+function objEvent ()
+{
+	const cone = objGroup.get(objPos[objTracker], 0);
+	
+	if (objTracker > 19) return;
+	if (!cone) return;
+	
+	cone.setActive(true);
+	cone.setVisible(true);
+	cone.setScale(0.2);
+	
+	objTracker++;
+}
 
-        player.anims.play('turn');
-    }
+function dropMoneyEvent ()
+{
+	const money = moneyGroup.get(player.x, player.y);
+	
+	if (moneyTracker > 9) return;
+	if (!money) return;
+	
+	moneyPos[moneyTracker] = money.x;
+	//DEBUGtext.setText('Money positions: ' + moneyPos[0] + moneyPos[1] + moneyPos[2] + moneyPos[3] + moneyPos[4] + moneyPos[5] + moneyPos[6] + moneyPos[7] + moneyPos[8] + moneyPos[9]);
+	
+	money.setActive(true);
+	money.setVisible(true);
+	
+	moneyTracker++;
+}
+
+function spawnMoneyEvent ()
+{
+	const money = moneyGroup.get(moneyPos[moneyTracker], 0);
+	
+	if (moneyTracker > 9) return;
+	if (!money) return;
+	
+	//DEBUGtext.setText('Money positions: ' + moneyPos[0] + moneyPos[1] + moneyPos[2] + moneyPos[3] + moneyPos[4] + moneyPos[5] + moneyPos[6] + moneyPos[7] + moneyPos[8] + moneyPos[9]);
+	
+	money.setActive(true);
+	money.setVisible(true);
+	
+	moneyTracker++;
+}
+
+function crash ()
+{
+	if (gameStep === 1 || gameStep === 4) {
+		gameStep++;
+		player.setVelocityX(0);
+		timedEvent1.remove(false);
+		timedEvent2.remove(false);
+		roadGif.setVisible(false);
+		moneyTracker = 0;
+		objTracker = 0;
+		maxTime = timer + 50;
+		timer = 0;
+	}
+}
+
+function collectCoin ()
+{
+	if (gameStep === 4) {
+		moneyGroup.clear(true);
+		score += 100;
+		DEBUGtext.setText(score);
+		//increase score
+	}
 }
